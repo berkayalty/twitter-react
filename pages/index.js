465fbd62fb8e46815d4e7e78909042057b9bdcc7
berkayalty <altayberkay1@gmail.com>
2021-02-10 22:19:08 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Button from "../components/button.js";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js lan!</h1>
+      <Button>Merhaba</Button>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ stories/Button.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Button from "../components/button";
+
+export default {
+  title: "Buttons",
+  component: Button,
+};
+
+export const SaveButton = () => <Button>Save</Button>;
